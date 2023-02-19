b5316969e6c5aedbe7c73ee7fbe4c7b1c15de5fd
hyochan.lee@gmail.com <hyochan.lee@gmail.com>
2023-02-19 19:25:00 +0900

    fix coc floating colors

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,6 +1,7 @@
-local g = vim.g
 local set = vim.opt
 local cmd = vim.cmd
+
+local Color, colors, Group, groups, styles = require('colorbuddy').setup()
 
 set.termguicolors = true
 cmd("highlight VertSplit guibg=0 guifg=White")
@@ -11,6 +12,11 @@ colorscheme everforest
 let g:everforest_better_performance = 1
 ]])
 
+Color.new('floating_fg', '#d3c6aa')
+Color.new('floating_bg', '#2d363b')
+Group.new('CocFloating', colors.floating_fg, colors.floating_bg, nil)
+
+-- local g = vim.g
 -- g.oceanic_next_terminal_bold = 0
 -- cmd([[
 -- syntax enable
